16f25aafb8b379fa27a35280166146658a5f9025
albert <herindrainyall2003@gmail.com>
2023-08-09 11:49:23 +0200

    back

--- deleteproduit.js
+++ deleteproduit.js
@@ -1,11 +1,14 @@
 const cron = require('node-cron');
 const pool = require('./config/db');
-
+//pour le 10 seconde 
 // Planifier la tâche de fond pour s'exécuter toutes les minutes
-cron.schedule('*/10 * * * * *', async () => {
+// cron.schedule('*/10 * * * * *', async () => {
+//   try {
+//     const tenSecondsAgo = new Date(Date.now() - 10 * 1000); // Calculer l'heure il y a 10 secondes
+cron.schedule('0 */5 * * *', async () => {
   try {
-    const tenSecondsAgo = new Date(Date.now() - 10 * 1000); // Calculer l'heure il y a 10 secondes
-    const deleteQuery = 'DELETE FROM tasks WHERE created_at <= $1;';
+    const fiveHoursAgo = new Date(Date.now() - 5 * 60 * 60 * 1000); // Calculer l'heure il y a 5 heures  
+const deleteQuery = 'DELETE FROM tasks WHERE created_at <= $1;';
     const values = [tenSecondsAgo];
 
     await pool.query(deleteQuery, values);
